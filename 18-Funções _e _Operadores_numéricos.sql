3c0c69e36f17bc733c8ef3baf31778170b362a57
ingrid504 <ingmoroliveira3374@gmail.com>
2024-08-26 16:48:40 -0300

    Create 18-Funções _e _Operadores_numéricos.sql

---
+++ 18-Funções _e _Operadores_numéricos.sql
@@ -0,0 +1,19 @@
+/*Funções e Operadores numéricos SQL*/
+select 1;
+
+select 1 + 1; 
+
+select - (2);
+
+select 4/3;
+
+select 10 div 2; 
+
+select pow(2 , 3);
+
+select * from tb_cli
+where cli_nome like '%Ana%';
+
+/*Funções Data e Hora MySQL*/
+select now();
+select year('2020-09-21 23:33:25' );
