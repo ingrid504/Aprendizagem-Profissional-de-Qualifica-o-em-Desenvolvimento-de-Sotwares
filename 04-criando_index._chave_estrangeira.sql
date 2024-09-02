513d11553982bc1717b58211b7f552868e8eb3a0
Ingrid <ingmoroliveira3374@gmail.com>
2024-09-02 14:58:13 -0300

    Update 04-criando_index._chave_estrangeira.sql

--- 04-criando_index._chave_estrangeira.sql
+++ 04-criando_index._chave_estrangeira.sql
@@ -1,54 +1,14 @@
- /*Criando novo usuario no servidor*/
-create user 'lara.rodrigues'@'localhost' identified by '123456';
-create user 'izabella.oliveira'@'localhost' identified by '123456';
-create user 'luane.gabrielly'@'localhost' identified by '123456';
-create user 'danielgomes'@'localhost' identified by '123456';
-create user 'tiago.lime'@'localhost' identified by '123456';
+/*Índice Criando*/
+alter table `bd_caso_estudo_vendas`.`tb_func`
+add index `fk_func_depto_idx` (`func_depto` ASC);
 
-/*Aplicando permissão de consulta de todos os arquivo banco no servidor*/
-grant select 
-on .
-to danielgomeslocalhost
-/*Aplicando permissão de banco inserção em todas as tabelas do banco*/
-grant insert 
-on bd_caso_estudos_vendas.*
-to estagiario@localhost;
- /*Aplicando permissão global de todos a arquivos/banco no servidor*/
-grant select 
-on .
-to danielgomeslocalhost
-/*Aplicando permissões de banco de inserção e atualização em  tabela definida*/
-grant insert 
-on bd_caso_estudos_vendas.*
-to danielgomeslocalhost;
-grant insert 
-on bd_caso_estudos_vendas.tb_prod
-to danielgomeslocalhost;
-grant 
-    	 on select (cli_cod,cli_nome,cli_cpf),
-   update(cli_end_cep)
-on bd_caso_estudos_vendas.tb_cli
-to danielgomeslocalhost;
-/**/
-grant insert,update,delete
-on `bd_caso_estudos_vendas.tb_func`
-to root@localhost;
-
-revoke insert,update
-on `bd_caso_estudos_vendas.tb_func`
-from root@localhost;
-
-grant delete
-on `bd_caso_estudos_vendas.tb_func`
-to root@localhost;
-
-revoke select 
-on `bd_caso_estudos_vendas.tb_func`
-from root@localhost;
-
-grant insert 
-on `bd_caso_estudos_vendas.tb_func`
-to root@localhost;
+/*Criando a chave  estrangeira*/
+alter table `bd_caso_estudo_vendas`.`tb_func`
+add constraint `fk_func_depto`
+foreign key (`func_depto`)
+references `bd_caso_estudo_vendas`.`tb_depto`(`depto_cod`)
+on delete no action
+on update no action;
 
 
-
+use bd_caso_estudo_vendas
